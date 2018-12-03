e29c09b56cdf6e2d9efccc677deeb866d9af123a
sonnth <sonnth.humg@gmail.com>
2018-12-03 09:23:07 +0700

    Initial commit!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ReadBooksComponent} from './read-books/read-books.component';
+import {ReadingBooksComponent} from './reading-books/reading-books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'reading-books',
+    component: ReadingBooksComponent
+  },
+  {
+    path: 'read-books',
+    component: ReadBooksComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReadingBooksComponent } from './reading-books/reading-books.component';
+import { ReadBooksComponent } from './read-books/read-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReadingBooksComponent,
+    ReadBooksComponent
   ],
   imports: [
     BrowserModule,
